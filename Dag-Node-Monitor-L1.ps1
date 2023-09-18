8440a3069f459aab22b5d7a48f5a1d54ff5acc56
gnon17 <105109259+gnon17@users.noreply.github.com>
2023-09-18 09:09:10 -0500

    Add files via upload

---
+++ Dag-Node-Monitor-L1.ps1
@@ -0,0 +1,55 @@
+#Variables - Get NodeInfo and Current State
+$NodeIP = "YOUR NODE PUBLIC IP"
+$discordhook = "YOUR WEBHOOK URL"
+$MyNode = invoke-webrequest https://nebula-apim.azure-api.net/public/dag/nodestate/integrationnet/"$NodeIP"?layer=l1 -UseBasicParsing | Select -ExpandProperty Content | Out-String -ErrorAction SilentlyContinue
+$NodeInfo = $MyNode.split(",") -replace '[{}""]'
+$nodestate = $MyNode.split(",")[-1] -replace "}"
+$Loop = $true
+
+#Actions based on node state
+If ($nodestate -match "Ready") {
+Write-Output "Node is in the Ready State"
+exit 0
+}
+else {
+    Write-output "Node not in the ready state. Current Node State is: $NodeState"
+    $payload = [pscustomobject]@{
+        content = "NODE IS NOT IN READY STATE
+        $nodestate"
+        }
+        Invoke-RestMethod -Uri $discordhook -Method Post -Body ($payload | ConvertTo-Json) -ContentType 'Application/Json'
+        #$From = "youremail@gmail.com"
+        #$To = "yourrecipientemail@outlook.com"
+        #$Subject = "NODE L1 DOWN"
+        #$Body = "L1 Node status is not Ready
+        #Current Status is $nodestate"
+        #$Password = "your app password" | ConvertTo-SecureString -AsPlainText -Force
+        #$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
+        #Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer "smtp.gmail.com" -port 587 -UseSsl -Credential $Credential
+    While ($Loop) {
+        $MyNode = invoke-webrequest https://nebula-apim.azure-api.net/public/dag/nodestate/integrationnet/"$NodeIP"?layer=l1 -UseBasicParsing | Select -ExpandProperty Content | Out-String -ErrorAction SilentlyContinue
+        $NodeInfo = $MyNode.split(",") -replace '[{}""]'
+        $nodestate = $MyNode.split(",")[-1] -replace "}"
+        If ($NodeState -notmatch "Ready")
+        {
+            Start-Sleep -Seconds 60
+        }
+        else {
+            $Loop = $false
+            Write-Output "Node Status is now ready"
+            $payload = [pscustomobject]@{
+                content = "NODE IS BACK ONLINE AND READY
+                $nodestate"
+                }
+            Invoke-RestMethod -Uri $discordhook -Method Post -Body ($payload | ConvertTo-Json) -ContentType 'Application/Json'
+            #$From = "youremail@gmail.com"
+            #$To = "yourrecipientemail@outlook.com"
+            #$Subject = "NODE L1 BACK ONLINE"
+            #$Body = "L1 Node Online and Ready
+            #Current Status is $nodestate"
+            #$Password = "your app password" | ConvertTo-SecureString -AsPlainText -Force
+            #$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
+            #Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer "smtp.gmail.com" -port 587 -UseSsl -Credential $Credential
+        }
+    }
+}
